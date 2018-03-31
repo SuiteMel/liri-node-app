52cb91466fb60376326ad06db5090f92e549030e
Melissa Waltner <melissamwaltner@gmail.com>
2018-03-31 13:28:11 -0500

    Implimented do-what-it-says command and updated readme with directions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # liri-node-app
+
+LIRI is a command line node app that takes in parameters and gives you back data. This data will also be logged to a log.txt file for you future reference.
+
+The commands that you can run in LIRI and what they do are as follows:
+
+'my-tweets' - This will display the last 20 tweets by the user SuiteMule.
+
+'spotify-this-song <song name here>' - This will display information about the first returned search result of the song you inputted. If you leave the search parameter blank it will search for the default song.
+
+'movie-this <movie name here>' - This will display information about the movie you searched for. If you leave the search parameter blank it will search for the default movie.
+
+do-what-it-says - This will run the command that has been written in the random.txt document.
+
+Remember, every output will be added to the log.txt file for you future reference.
+
+## How to get started
+This app uses node.js and npm. In your console type 'npm install' to aquire the required node libraries to run this app. You will also need to add your own .env file with your personal keys for twitter and spotify in order to utilize their apis and utilize the 'my-tweets' and 'spotify-this-song' commands.
--- liri.js
+++ liri.js
@@ -10,25 +10,32 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var param = process.argv[2];
+var input = process.argv.slice(3);
+var filler = "\n----------------\n";
 
-switch (param) {
-  case "my-tweets":
-    myTweets();
-    break;
-  case "spotify-this-song":
-    spotifySong();
-    break;
-  case "movie-this":
-    movieThis();
-    break;
-  case "do-what-it-says":
-    doIt();
-    break;
-  default:
-    console.log("This command was not recognized");
-    console.log("Valid commands are 'my-tweets', 'spotify-this-song', 'movie-this', and 'do-what-it-says'.");
-    break;
+commands(param, input);
+
+function commands(param, term) {
+  switch (param) {
+    case "my-tweets":
+      myTweets();
+      break;
+    case "spotify-this-song":
+      spotifySong(term);
+      break;
+    case "movie-this":
+      movieThis(term);
+      break;
+    case "do-what-it-says":
+      doIt();
+      break;
+    default:
+      console.log("This command was not recognized");
+      console.log("Valid commands are 'my-tweets', 'spotify-this-song', 'movie-this', and 'do-what-it-says'.");
+      break;
+  }
 }
+
 
 function myTweets() {
   var twitterUser = { screen_name: 'SuiteMule' };
@@ -37,26 +44,29 @@ function myTweets() {
 
     if (!error) {
       console.log(twitterUser.screen_name + "'s Recent Tweets");
-      if (size > 9) {
-        var size = 9;
+      if (size > 20) {
+        var size = 20;
       }
     }
-
+    var feed = [];
     for (i = 0; i < size; i++) {
       var text = tweets[i].text;
       var time = tweets[i].created_at;
-      console.log(text + " Posted: " + time);
-      console.log("----------------");
+      feed.push(text + " Posted: " + time + filler)
+
     }
+
+    logIt(feed.join(" "));
   })
 }
 
-function spotifySong() {
-  var search = process.argv.slice(3).join(" ");
+function spotifySong(input) {
+  var search = input.join(" ");
 
   if (search === "") {
     var search = "The Sign by Ace of Base";
   }
+
   spotify.search({ type: 'track', query: search, limit: 1 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -67,48 +77,56 @@ function spotifySong() {
     var song = music[0].name;
     var album = music[0].album.name;
     var preview = music[0].preview_url;
-    console.log(song + " by " + artist + " from the album " + album);
-    console.log("Listen here: " + preview);
+    logIt(song + " by " + artist + " from the album " + album + "\nListen here: " + preview + filler);
   });
 }
 
-function movieThis() {
-  var search = process.argv.slice(3).join("+");
+function movieThis(input) {
+  var search = input.join("+");
 
   if (search === "") {
     var search = "Mr. Nobody";
   }
 
-  request("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var result = JSON.parse(body);
-    var title = result.Title;
-    var year = result.Year;
-    var imdbRating = result.imdbRating;
-    var tomatoRating = result.Ratings[1].Value;
-    var country =  result.Country;
-    var lang = result.Language;
-    var plot = result.Plot;
-    var actors = result.Actors;
-    console.log(title + " (" + year + ")");
-    console.log(plot);
-    console.log("Featuring " + actors);
-    console.log("Languages: " + lang + ". Countries: " + country);
-    console.log("Ratings: IMDB " + imdbRating + ", Rotten Tomatoes " + tomatoRating);
-    
-  }
-});
+  request("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var result = JSON.parse(body);
+      var title = result.Title;
+      var year = result.Year;
+      var imdbRating = result.imdbRating;
+      var tomatoRating = result.Ratings[1].Value;
+      var country = result.Country;
+      var lang = result.Language;
+      var plot = result.Plot;
+      var actors = result.Actors;
+      logIt(title + " (" + year + ")\n" + plot + "\nFeaturing " + actors + "\nLanguages: " + lang + ". Countries: " + country + "\nRatings: IMDB " + imdbRating + ", Rotten Tomatoes " + tomatoRating + filler);
+    }
+  });
 }
 
 function doIt() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
+  fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
       return console.log(error);
     }
-  
+
     var dataArr = data.split(",");
     param = dataArr[0];
-    console.log(dataArr[0] + " " + dataArr[1]);
-   
+    input = [dataArr[1]];
+    commands(param, input);
+  });
+}
+
+function logIt(output) {
+  fs.appendFile("log.txt", output, function (err) {
+
+    if (err) {
+      console.log(err);
+    }
+
+    else {
+      console.log(output);
+    }
+
   });
 }
